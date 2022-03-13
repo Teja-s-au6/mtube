fc34610a1306bf4ec4cb25096108a6f7cf84e13d
Tejas <tejaspvg@gmail.com>
2022-03-13 09:46:33 +0530

    removed consoles

--- src/components/Replies.jsx
+++ src/components/Replies.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReplyListItem from './ReplyListItem';
 
 const Replies = ({replies}) => {
-    //console.log(replies)
     return replies.comments.map(replyComment => <ReplyListItem  key={replyComment.id} replyComment={replyComment}/>)
 }
 
